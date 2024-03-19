4f7e7febd2b244981d61e355f84ec23216c305e6
Felipe <felipecerboncinix@gmail.com>
2024-03-18 22:47:22 -0300

    Initial commit

---
+++ src/main/java/com/cerbon/keep_stepping/Keep_Stepping.java
@@ -0,0 +1,12 @@
+package com.cerbon.keep_stepping;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Keep_Stepping.MOD_ID)
+public class Keep_Stepping {
+
+    public static final String MOD_ID = "keep_stepping";
+
+    public Keep_Stepping() {}
+
+}
