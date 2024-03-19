bbd09aedfd17ebe7fddc62a0be81c52428c59827
Felipe <felipecerboncinix@gmail.com>
2024-03-18 22:49:29 -0300

    Rename main class

--- src/main/java/com/cerbon/keep_stepping/KeepStepping.java
+++ src/main/java/com/cerbon/keep_stepping/KeepStepping.java
@@ -2,11 +2,11 @@ package com.cerbon.keep_stepping;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(Keep_Stepping.MOD_ID)
-public class Keep_Stepping {
+@Mod(KeepStepping.MOD_ID)
+public class KeepStepping {
 
     public static final String MOD_ID = "keep_stepping";
 
-    public Keep_Stepping() {}
+    public KeepStepping() {}
 
 }
